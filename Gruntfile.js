85de69391ac4d65d5b4a7d4c7e45e92673af6e54
Grigore Alex <grigore.alex22@gmail.com>
2015-10-06 11:17:05 +0300

    Added Theme Customizer for header images and logo, added some initial configuration for the menu block

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function (grunt) {
 
 			dist: {
 				options: {
-					outputStyle: 'compressed'
+					outputStyle: 'expanded'
 				},
 				files: {
 					'assets/stylesheets/foundation.css': 'assets/scss/foundation.scss'
--- functions.php
+++ functions.php
@@ -42,6 +42,9 @@ require_once( 'library/enqueue-scripts.php' );
 /** Add theme support */
 require_once( 'library/theme-support.php' );
 
+/** Add theme Logo Support */
+require_once( 'library/theme-customizer.php' );
+
 /** Add Nav Options to Customer */
 require_once( 'library/custom-nav.php' );
 
