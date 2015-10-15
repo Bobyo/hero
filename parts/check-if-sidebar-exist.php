f23775e10c1a972e014ce04a60dd737b41b2e3b4
Grigore Alex <grigore.alex22@gmail.com>
2015-10-15 16:34:39 +0300

    Controls for the social links, controls for the sidebar hide/show, font replacement, various improvements to the styling

--- parts/check-if-sidebar-exist.php
+++ parts/check-if-sidebar-exist.php
@@ -1,9 +1,8 @@
 <?php
   // Display a suitable column layout depending on whether sidebar exists
-  if ( is_active_sidebar( 'sidebar-widgets' ) ) :
+  if ( is_active_sidebar( 'sidebar-widgets' ) && hero_options( 'hero_hide_sidebar' ) == '0') :
     echo '<div class="small-12 large-8 columns" role="main">';
-
   else :
-    echo '<div class="small-12 columns" role="main">';
+    echo '<div class="small-12 large-8 large-push-2 columns" role="main">';
   endif;
 ?>
