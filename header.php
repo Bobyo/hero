0791fbc79a3bf0d217d5680d3a2734fa9b5886b2
Grigore Alex <grigore.alex22@gmail.com>
2015-11-01 17:11:32 +0200

    Included the original stylesheet in the build

--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114-precomposed.png">
 		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72-precomposed.png">
 		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-precomposed.png">
+        <link ref="text/stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" />
 
 		<?php wp_head(); ?>
 	</head>
